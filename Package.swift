b1033063a87ef8a497e155954eeb167cfdf283a6
Tuan Nguyen Anh <tuannguyenanh177@gmail.com>
2022-11-08 16:11:18 +0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
             targets: ["LottieUI"]),
     ],
     dependencies: [
-        .package(name: "Lottie", url: "https://github.com/airbnb/lottie-ios.git", exact: "3.4.4")
+        .package(url: "https://github.com/airbnb/lottie-ios.git", exact: "3.4.4")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
